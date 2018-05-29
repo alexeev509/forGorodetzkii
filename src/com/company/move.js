88f4e7ef281de144c25c8af34b92d2dd06762fba
Alexeev Yaroslav <ice-t95@mail.ru>
2018-05-29 12:54:20 +0300

    changes

--- src/com/company/move.js
+++ src/com/company/move.js
@@ -1,13 +1,25 @@
 
-var maxCountOfCars=7;
+var maxCountOfCars=6;
 var minCountOfCars=1;
 var maxdeltaBetweenCars=130;
 var mindeltaBetweenCars=80;
 var deltaLenBetweenCars=0;
 var countOfCars=0;
-var timeOfTrafficLights;
+var stateOfTrafficLights=0;
+//0 - green
+//1 - yellow
+//2 - red
 var massOfFirsBandCars=[];
 var massOfSecondBandCars=[];
+var massOfThirdBandCars=[];
+
+var currentId=0;
+//0-forward
+
+//1-right
+//2-left
+
+var massOfDirections=[];
 
 function createCars() {
     createCarsForEveryBand("startPositionFirst");
@@ -16,6 +28,16 @@ function createCars() {
 
 }
 
+function directionOfCars(e) {
+    var punktOfList=document.getElementById("directionList");
+    var list=document.getElementById("directionList");
+    list.style.visibility="visible";
+    currentId=e.target.id;
+    if(massOfDirections[currentId]==0 || massOfDirections[currentId]==1 ||massOfDirections[currentId]==2 ) {
+        punktOfList.options[massOfDirections[currentId]].selected =true ;
+    }
+
+}
 function createCarsForEveryBand(nameOfClass) {
     countOfCarsForThirdBand=Math.floor(Math.random()*(maxCountOfCars-minCountOfCars)+minCountOfCars);
     for(var i=0;i<countOfCarsForThirdBand;i++) {
@@ -30,15 +52,30 @@ function createCarsForEveryBand(nameOfClass) {
         if(nameOfClass=="startPositionFirst") {
             massOfFirsBandCars[i] = createCar;
         }
-        if(nameOfClass=="startPositionSecond"){
+        else if(nameOfClass=="startPositionSecond"){
             massOfSecondBandCars[i]=createCar;
         }
+        else {
+            massOfThirdBandCars[i]=createCar;
+        }
         countOfCars++;
+    }
+    stateOfTrafficLights=Math.floor(Math.random()*(2-0)+0);
+    changeColorOfTrafficLight();
+    var massOfCars=document.getElementsByClassName("circle");
+    for(var i=0;i<massOfCars.length;i++){
+        massOfCars[i].addEventListener("click",directionOfCars);
+        massOfCars[i].style.textAlign="center";
+        massOfCars[i].innerHTML=massOfCars[i].id;
+    }
+    //All car go forward if we dont choose direction
+    for(var i=0;i<massOfCars.length;i++){
+        massOfDirections[i]=0;
     }
 }
 
 function start() {
-    //window.setInterval(changeColorOfTrafficLight,1000);
+    window.setInterval(changeColorOfTrafficLight,2000);
     for(var i=0; i<countOfCars; i++) {
         var timerMulti = window.setInterval( function(j)
         {
@@ -51,29 +88,45 @@ function run(carId){
     var carExamplar=document.getElementById(carId);
     console.log(carExamplar.className)
     var direction=window.getComputedStyle(carExamplar,null);
-    if(carExamplar.className=="circle startPositionSecond" && parseInt(direction.left)>=687){
+    if(carExamplar.className=="circle startPositionSecond" && parseInt(direction.left)>=687 && massOfDirections[carId]==2){
         if(lengthAnotherCarsToCentre()) {
             carExamplar.style.top = parseInt(direction.top) - 15 + 'px';
         }
     }else {
-        if(lenghtBetweenCars(carExamplar)) {
+        if(lenghtBetweenCars(carExamplar) && colorOfTrafficLight(carExamplar)) {
             carExamplar.style.left = parseInt(direction.left) + 10 + 'px';
         }
     }
 }
 
-// function changeColorOfTrafficLight(){
-//     var w=document.getElementsByClassName("greenRectangles right circle trafficLight");
-//     w.style.color="black";
-//     w.style.backgroundColor="white";
-//     w.innerHTML='50';
-// }
+function changeColorOfTrafficLight(){
+    var green=document.getElementsByClassName("trafficLight trafficLightFirst");
+    var yellow=document.getElementsByClassName("trafficLight trafficLightSecond");
+    var red=document.getElementsByClassName("trafficLight trafficLightThird");
+    if(stateOfTrafficLights==0){
+        green[0].style.backgroundColor="green";
+        yellow[0].style.backgroundColor="white";
+        red[0].style.backgroundColor="white";
+        stateOfTrafficLights=1;
+
+    }else if(stateOfTrafficLights==1){
+        green[0].style.backgroundColor="white";
+        yellow[0].style.backgroundColor="yellow";
+        red[0].style.backgroundColor="white";
+        stateOfTrafficLights=2;
+    }else {
+        green[0].style.backgroundColor="white";
+        yellow[0].style.backgroundColor="white";
+        red[0].style.backgroundColor="red";
+        stateOfTrafficLights=0;
+    }
+}
 
 function lengthAnotherCarsToCentre() {
     var bb=true;
     for(var i=0;i<massOfFirsBandCars.length;i++){
         var direction=window.getComputedStyle(massOfFirsBandCars[i],null);
-        if(parseInt(direction.left)>400 && parseInt(direction.left)<760){
+        if(parseInt(direction.left)>430 && parseInt(direction.left)<760){
             bb=false;
         }
     }
@@ -84,27 +137,57 @@ function lenghtBetweenCars(targetCar) {
     var lenghtLess60=true;
     var directionOfTarget=window.getComputedStyle(targetCar,null);
     if(targetCar.className=="circle startPositionFirst"){
-        
+        if(parseInt(targetCar.id)!=massOfFirsBandCars.length-1){
+            //расстояние до впереди стоящей машины
+            directionOfAnotherCar=window.getComputedStyle(massOfFirsBandCars[parseInt(targetCar.id)+1],null);
+            if(Math.abs(parseInt(directionOfTarget.left)-parseInt(directionOfAnotherCar.left))<80){
+                lenghtLess60=false;
+            }
+        }
     }
     else if(targetCar.className=="circle startPositionSecond"){
-        console.log("middle")
-        // for(var j=0;j<massOfSecondBandCars.length;j++){
-        //     directionOfAnotherCar=window.getComputedStyle(massOfSecondBandCars[j],null);
-        //     if(Math.abs(parseInt(directionOfTarget.left)-parseInt(directionOfAnotherCar.left))<60 && targetCar.id!=massOfSecondBandCars[j].id){
-        //         lenghtLess80=false;
-        //         break;
-        //     }
-        // }
 
         if(parseInt(targetCar.id)!=massOfFirsBandCars.length+massOfSecondBandCars.length-1){
             //расстояние до впереди стоящей машины
             directionOfAnotherCar=window.getComputedStyle(massOfSecondBandCars[parseInt(targetCar.id)-massOfFirsBandCars.length+1],null);
-            if(Math.abs(parseInt(directionOfTarget.left)-parseInt(directionOfAnotherCar.left))<60
+            if(Math.abs(parseInt(directionOfTarget.left)-parseInt(directionOfAnotherCar.left))<80
                 && Math.abs(parseInt(directionOfTarget.top)-Math.abs(parseInt(directionOfAnotherCar.top)))<5
                 ){
                         lenghtLess60=false;
             }
         }
+    }else {
+        if(parseInt(targetCar.id)!=massOfFirsBandCars.length+massOfSecondBandCars.length+massOfThirdBandCars.length-1){
+            //расстояние до впереди стоящей машины
+            directionOfAnotherCar=window.getComputedStyle(massOfThirdBandCars[parseInt(targetCar.id)-massOfFirsBandCars.length-massOfSecondBandCars.length+1],null);
+            if(Math.abs(parseInt(directionOfTarget.left)-parseInt(directionOfAnotherCar.left))<80){
+                lenghtLess60=false;
+            }
+        }
     }
+
     return lenghtLess60;
 }
+
+function colorOfTrafficLight(targetCar) {
+    bb=true;
+    var direction=window.getComputedStyle(targetCar,null);
+    if(stateOfTrafficLights==0 && parseInt(direction.left)>560 && parseInt(direction.left)<600 && parseInt(direction.top)>320 ){
+        bb=false
+    }
+    return bb;
+}
+
+
+function onSelectList() {
+    var punktOfList=document.getElementById("directionList");
+    if(punktOfList.selectedIndex==0){
+        massOfDirections[currentId]=0;
+    }
+    else if(punktOfList.selectedIndex==1){
+        massOfDirections[currentId]=1;
+    }else {
+        massOfDirections[currentId]=2;
+    }
+   // alert(massOfDirections[currentId])
+ }
